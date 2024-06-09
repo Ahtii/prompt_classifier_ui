21398d2d413b2ba07789e09079b09ef06e3abe1c
Ahtii <ahtishamshafi9906@gmail.com>
2024-06-09 16:08:35 +0530

    added a basic chat interface

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chat-history">
+        <p className="canvas-title"> Customer Chat </p>
+        <div className="canvas-body">
+          <div className="canvas-text">
+            <div className="canvas-placeholder">
+              <p className="canvas-placeholder-title"> Want to classify your queries? </p>
+              <hr/>
+              <br/>
+              <p className="canvas-placeholder-body">
+                Enter your question or concern here, and let me assist you by categorizing it for a faster resolution.
+              </p>
+            </div>
+          </div>
+          <div className="canvas-action">
+            <input placeholder="Enter your prompt here"></input>
+            <button> Send </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
