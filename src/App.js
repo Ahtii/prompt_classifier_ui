df8287f23064dadf5e90e81f5073d0b5051b4fd6
Ahtii <ahtishamshafi9906@gmail.com>
2024-06-09 17:12:48 +0530

    Added event handler for message input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [show, setShow] = useState(true);
+
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    const messagesElement = document.querySelector('.messages');
+
+    setShow(inputValue === '' && messagesElement.innerHTML === '');
+  };
+
   return (
     <div className="App">
       <div className="chat-history">
         <p className="canvas-title"> Customer Chat </p>
         <div className="canvas-body">
           <div className="canvas-text">
-            <div className="canvas-placeholder">
+            <div className="canvas-placeholder" style={{ display: show ? 'block' : 'none' }}>
               <p className="canvas-placeholder-title"> Want to classify your queries? </p>
               <hr/>
               <br/>
@@ -16,9 +26,10 @@ function App() {
                 Enter your question or concern here, and let me assist you by categorizing it for a faster resolution.
               </p>
             </div>
+            <div className="messages"></div>
           </div>
           <div className="canvas-action">
-            <input placeholder="Enter your prompt here"></input>
+            <input placeholder="Enter your prompt here" onChange={handleInputChange}></input>
             <button> Send </button>
           </div>
         </div>
