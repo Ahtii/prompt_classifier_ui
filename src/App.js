43a05747ecc2c272d3a5787aeb8d0c4cd0ea7c67
Ahtii <ahtishamshafi9906@gmail.com>
2024-06-09 20:27:44 +0530

    Added spinner and integrated api

--- src/App.js
+++ src/App.js
@@ -12,11 +12,41 @@ function App() {
     setShow(inputValue === '' && messagesElement.innerHTML === '');
   };
 
+  const getParagraphChild = (text) => {
+    var paragraphElement = document.createElement("p");
+    paragraphElement.appendChild(document.createTextNode(text))
+    return paragraphElement
+  }
+
+  const handleSubmit = (event) => {
+    const prompt = document.querySelector('input').value;
+
+    var messages = document.querySelector('.messages');
+    messages.appendChild(getParagraphChild(prompt))
+
+    document.querySelector('.canvas-body .fa').style.display = "block";
+
+    fetch('http://127.0.0.1:8000/category/', {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(prompt)
+    })
+    .then(response => response.json())
+    .then(data => {
+       document.querySelector('.canvas-body .fa').style.display = "none";
+       messages.appendChild(getParagraphChild(data['category']))
+    });
+  };
+
   return (
     <div className="App">
       <div className="chat-history">
         <p className="canvas-title"> Customer Chat </p>
         <div className="canvas-body">
+          <i className="fa fa-spinner fa-spin"></i>
           <div className="canvas-text">
             <div className="canvas-placeholder" style={{ display: show ? 'block' : 'none' }}>
               <p className="canvas-placeholder-title"> Want to classify your queries? </p>
@@ -30,7 +60,7 @@ function App() {
           </div>
           <div className="canvas-action">
             <input placeholder="Enter your prompt here" onChange={handleInputChange}></input>
-            <button> Send </button>
+            <button onClick={handleSubmit}> Send </button>
           </div>
         </div>
       </div>
